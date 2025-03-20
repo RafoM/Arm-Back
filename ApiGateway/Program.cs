865f19ca56e198ef3e7a291c4d6c5387cfb74d90
Xrm <armanhovh26@gmail.com>
2025-03-20 17:35:25 +0400

    Bug fix

--- ApiGateway/Program.cs
+++ ApiGateway/Program.cs
@@ -44,39 +44,6 @@ builder.Services.AddAuthentication(options =>
 builder.Services.AddOcelot(builder.Configuration);
 builder.Services.AddSwaggerForOcelot(builder.Configuration);
 
-builder.Services.AddSwaggerGen(swagger =>
-{
-    swagger.SwaggerDoc("v1", new OpenApiInfo
-    {
-        Version = "v1",
-        Title = "Guide Microservice API"
-    });
-    swagger.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
-    {
-        Name = "Authorization",
-        Type = SecuritySchemeType.ApiKey,
-        Scheme = "Bearer",
-        BearerFormat = "JWT",
-        In = ParameterLocation.Header,
-        Description = "JWT Authorization header using the Bearer scheme. \r\n\r\n Enter 'Bearer' [space] and then your token in the text input below.\r\n\r\nExample: \"Bearer 12345abcdef\"",
-    });
-    swagger.AddSecurityRequirement(new OpenApiSecurityRequirement
-            {
-                {
-                      new OpenApiSecurityScheme
-                        {
-                            Reference = new OpenApiReference
-                            {
-                                Type = ReferenceType.SecurityScheme,
-                                Id = "Bearer"
-                            }
-                        },
-                        new string[] {}
-
-                }
-            });
-});
-
 var app = builder.Build();
 app.UseRouting();
 app.UseAuthentication();
